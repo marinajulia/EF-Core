d1921662ba153b26fa980cc53f091dccc2973ac9
marinajulia <marinajulia707@gmail.com>
2022-08-22 22:02:56 -0300

    save logs

--- Modulo Infraestrutura/Data/ApplicationContext.cs
+++ Modulo Infraestrutura/Data/ApplicationContext.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using Curso.Domain;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
@@ -8,6 +9,8 @@ namespace Curso.Data
 {
     public class ApplicationContext : DbContext
     {
+        //o append serve para não criar um arquivo novo toda vez, e sim reaproveitar o mesmo
+        private readonly StreamWriter _writer = new StreamWriter("meu_log_do_ef_core.txt", append: true);
         public DbSet<Departamento> Departamentos { get; set; }
         public DbSet<Funcionario> Funcionarios { get; set; }
 
@@ -17,11 +20,17 @@ namespace Curso.Data
             optionsBuilder
                 .UseSqlServer(strConnection)
                 // .LogTo(Console.WriteLine, LogLevel.Information);
-                .LogTo(Console.WriteLine, 
-                    new[] {CoreEventId.ContextInitialized, RelationalEventId.CommandExecuted},
-                    LogLevel.Information,
-                    DbContextLoggerOptions.LocalTime | DbContextLoggerOptions.SingleLine
-                );
+                // .LogTo(Console.WriteLine, 
+                //     new[] {CoreEventId.ContextInitialized, RelationalEventId.CommandExecuted},
+                //     LogLevel.Information,
+                //     DbContextLoggerOptions.LocalTime | DbContextLoggerOptions.SingleLine
+                // );
+                .LogTo(_writer.WriteLine, LogLevel.Information);
+        }
+
+        public override void Dispose(){
+            base.Dispose();
+            _writer.Dispose();
         }
        
     }
