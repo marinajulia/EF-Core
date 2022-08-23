6cfef8b3e4cb481fc5153ae7d470c0867604bd4b
marinajulia <marinajulia707@gmail.com>
2022-08-22 21:28:23 -0300

    Refactor code

---
+++ Modulo Procedures/Data/ApplicationContext.cs
@@ -0,0 +1,25 @@
+using System;
+using Curso.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace Curso.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Departamento> Departamentos { get; set; }
+        public DbSet<Funcionario> Funcionarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+            .UseSqlServer(@"Data Source=DESKTOP-RTPBNVC\SQLEXPRESS;Initial Catalog=Curso;Integrated Security=True;pooling=true;")
+            .EnableSensitiveDataLogging()
+            // .UseLazyLoadingProxies()
+            .LogTo(Console.WriteLine, LogLevel.Information);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder){
+            // modelBuilder.Entity<Departamento>().HasQueryFilter(p=>!p.Excluido);
+        }
+    }
+}
