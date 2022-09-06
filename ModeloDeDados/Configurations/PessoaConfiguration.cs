7d9d9cf31248d70238248ad54a7cd40164b0b0dc
marinajulia <marinajulia707@gmail.com>
2022-09-06 10:02:23 -0300

    chore: Modelo de dados TPT

--- ModeloDeDados/Configurations/PessoaConfiguration.cs
+++ ModeloDeDados/Configurations/PessoaConfiguration.cs
@@ -6,13 +6,42 @@ namespace Curso.Configurations
 {
     public class PessoaConfiguration : IEntityTypeConfiguration<Pessoa>
     {
+
+        //configuração para criar uma tabela só em formato de hierarquia TPH
+        // public void Configure(EntityTypeBuilder<Pessoa> builder)
+        // {
+        //     builder.ToTable("Pessoas")
+        //     .HasDiscriminator<int>("TipoPessoa")
+        //     .HasValue<Pessoa>(3)
+        //     .HasValue<Instrutor>(1)
+        //     .HasValue<Aluno>(2);
+        // }
+
+        //para gerar tabelas separadas TPT
         public void Configure(EntityTypeBuilder<Pessoa> builder)
         {
-            builder.ToTable("Pessoas")
-            .HasDiscriminator<int>("TipoPessoa")
-            .HasValue<Pessoa>(3)
-            .HasValue<Instrutor>(1)
-            .HasValue<Aluno>(2);
+            builder.ToTable("Pessoas");
         }
+        
+    }
+
+    public class InstrutorConfiguration : IEntityTypeConfiguration<Instrutor>
+    {
+
+        public void Configure(EntityTypeBuilder<Instrutor> builder)
+        {
+            builder.ToTable("Instrutores");
+        }
+        
+    }
+
+    public class AlunoConfiguration : IEntityTypeConfiguration<Aluno>
+    {
+
+        public void Configure(EntityTypeBuilder<Aluno> builder)
+        {
+            builder.ToTable("Alunos");
+        }
+        
     }
 }
