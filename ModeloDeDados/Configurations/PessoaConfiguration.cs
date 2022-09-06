dd450316a3b8555b94ca67b75b441640b3cd40e7
marinajulia <marinajulia707@gmail.com>
2022-09-06 08:55:11 -0300

    Modelo de dados com TPH

---
+++ ModeloDeDados/Configurations/PessoaConfiguration.cs
@@ -0,0 +1,18 @@
+using Curso.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Curso.Configurations
+{
+    public class PessoaConfiguration : IEntityTypeConfiguration<Pessoa>
+    {
+        public void Configure(EntityTypeBuilder<Pessoa> builder)
+        {
+            builder.ToTable("Pessoas")
+            .HasDiscriminator<int>("TipoPessoa")
+            .HasValue<Pessoa>(3)
+            .HasValue<Instrutor>(1)
+            .HasValue<Aluno>(2);
+        }
+    }
+}
--- ModeloDeDados/Program.cs
+++ ModeloDeDados/Program.cs
@@ -24,7 +24,44 @@ namespace DominandoEFCore
             // TrabalhandoComPropriedadeDeSombra();
             // Relacionamento1Para1();
             // RelacionamentoMuitosParaMuitos();
-            CampoDeApoio();
+            // CampoDeApoio();
+            ExemploTPH();
+        }
+        static void ExemploTPH()
+        {
+            using (var db = new ApplicationContext())
+            {
+                db.Database.EnsureDeleted();
+                db.Database.EnsureCreated();
+
+                var pessoa = new Pessoa{Nome = "Pessoa1"};
+                var Instrutor = new Instrutor{Nome = "Pessoa2", Tecnologia = ".net", Desde = DateTime.Now};
+                var aluno = new Aluno{Nome = "Pessoa3", Idade = 21, DataContrato = DateTime.Now};
+
+                db.AddRange(pessoa, Instrutor, aluno);
+                db.SaveChanges();
+
+                var pessoas = db.Pessoas.AsNoTracking().ToArray();
+                var instrutores = db.Instrutores.AsNoTracking().ToArray();
+                // var alunos = db.Alunos.AsNoTracking().ToArray();
+                var alunos = db.Pessoas.OfType<Aluno>().AsNoTracking().ToArray();
+
+                Console.WriteLine("Pessoas*************************");
+
+                foreach(var p in pessoas){
+                    Console.WriteLine($"Id: {p.Id} -> {p.Nome}");
+                }
+
+                Console.WriteLine("Instrutores*************************");
+                foreach(var p in instrutores){
+                    Console.WriteLine($"Id: {p.Id} -> {p.Nome}, tecnologia: {p.Tecnologia}");
+                }
+
+                Console.WriteLine("Alunos*************************");
+                foreach(var p in alunos){
+                    Console.WriteLine($"Id: {p.Id} -> {p.Nome}, data: {p.DataContrato}");
+                }
+            }
         }
         static void CampoDeApoio()
         {
