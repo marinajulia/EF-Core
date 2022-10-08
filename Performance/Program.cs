eba43a3a97384545d1cfd293c9c380b8f32ab0c1
marinajulia <marinajulia707@gmail.com>
2022-10-08 17:14:30 -0300

    chore: Initialize tracking and asNoTracking

---
+++ Performance/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using Curso.Data;
+using Curso.Domain;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace DominandoEFCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+
+      
+    }
+
