8cd40172c647533a329f61f972e96f596294a7f9
marinajulia <marinajulia707@gmail.com>
2022-10-18 20:50:50 -0300

    chore: Initialize UoW

---
+++ Uow/src/Data/ApplicationContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using src.Domain;
+
+namespace src.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Departamento> Departamentos { get; set; }
+        public DbSet<Colaborador> Colaboradores { get; set; }
+    }
+}
